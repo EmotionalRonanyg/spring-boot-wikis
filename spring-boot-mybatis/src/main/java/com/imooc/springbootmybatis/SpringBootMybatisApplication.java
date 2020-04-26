11c569172682a563963ad8e91798fe5b232714e7
studyingpanda <chenwenyang1990@163.com>
2020-04-26 23:10:06 +0800

    chen

--- spring-boot-mybatis/src/main/java/com/imooc/springbootmybatis/SpringBootMybatisApplication.java
+++ spring-boot-mybatis/src/main/java/com/imooc/springbootmybatis/SpringBootMybatisApplication.java
@@ -1,9 +1,11 @@
 package com.imooc.springbootmybatis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.imooc.springbootmybatis") // 指定MyBatis扫描的包，以便将数据访问接口注册为bean
 public class SpringBootMybatisApplication {
 
 	public static void main(String[] args) {
