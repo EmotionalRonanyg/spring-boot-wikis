19a14a55b3a1bb0d4b649609e232fd9bdf907d08
studyingpanda <chenwenyang1990@163.com>
2020-05-26 09:37:06 +0800

    chen

--- spring-boot-aop/src/main/java/com/imooc/springbootaop/ResultBo.java
+++ spring-boot-aop/src/main/java/com/imooc/springbootaop/ResultBo.java
@@ -1,6 +1,24 @@
 package com.imooc.springbootaop;
 
 public class ResultBo<T> {
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
 	/**
 	 * 错误码 0表示没有错误(异常) 其他数字代表具体错误码
 	 */
